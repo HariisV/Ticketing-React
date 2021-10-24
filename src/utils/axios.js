8527cd742a3df9d6113aa4a98b3dbc652e395849
Haris Wahyudi <hariswh9@gmail.com>
2021-10-24 23:30:19 +0700

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // ENV
 const axiosApiInterface = axios.create({
-  baseURL: "http://localhost:3001/"
+  baseURL: "http://tickez.herokuapp.com/"
 });
 
 axiosApiInterface.interceptors.request.use(
